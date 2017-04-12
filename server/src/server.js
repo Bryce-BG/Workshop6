6889d172aae8889fe6d32676be4c00efbba882e2
Bryce Bodley-Gomes <bbodleygomes@umass.edu>
2017-04-12 15:52:34 -0400

    static

--- server/src/server.js
+++ server/src/server.js
@@ -2,8 +2,8 @@
 var express = require('express');
 //STUDENT CODE
 
-var Util = require('./util');
-var reverseString = Util.reverseString; //import the reverseString function
+//var Util = require('./util');
+//var reverseString = Util.reverseString; //import the reverseString function
 
 
 //END STUDENT KING
@@ -12,11 +12,11 @@ var reverseString = Util.reverseString; //import the reverseString function
 
 // Creates an Express server.
 var app = express();
-
+app.use(express.static('../client/build'));
 // Defines what happens when it receives the `GET /` request
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+// app.get('/', function (req, res) {
+//   res.send('Hello World!');
+// });
 
 // Starts the server on port 3000!
 app.listen(3000, function () {
@@ -28,15 +28,15 @@ app.listen(3000, function () {
 var bodyParser = require('body-parser'); //import body parser
 app.use(bodyParser.text());
 // Handle POST /reverse [data]
-app.post('/reverse', function (req, res) {
-  // If the request came with text, then the text() middleware handled it
-  // and made `req.body` a string.
-  // Check that req.body is a string.
-  if (typeof(req.body) === 'string') {
-    var reversed = reverseString(req.body);
-    res.send(reversed);
-  } else {
-    // POST did not contain a string. Send an error code back!
-    res.status(400).end() //#what does end() do?
-  }
-});
+// app.post('/reverse', function (req, res) {
+//   // If the request came with text, then the text() middleware handled it
+//   // and made `req.body` a string.
+//   // Check that req.body is a string.
+//   if (typeof(req.body) === 'string') {
+//     var reversed = reverseString(req.body);
+//     res.send(reversed);
+//   } else {
+//     // POST did not contain a string. Send an error code back!
+//     res.status(400).end() //#what does end() do?
+//   }
+// });
